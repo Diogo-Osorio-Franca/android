2179294a5fd5b4b31506fba033f071a442ce67b7
Diogo-Osorio-Franca <91437130+Diogo-Osorio-Franca@users.noreply.github.com>
2023-03-29 09:42:50 -0300

    Add files via upload

--- src/main/java/com/example/myapplication/MainActivity.kt
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,53 +1,47 @@
 package com.example.myapplication
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var users: MutableList<String> = mutableListOf<String>("user1", "user2")
-    var emails: MutableList<String> = mutableListOf<String>("email1", "email2")
-    var passwords: MutableList<String> = mutableListOf<String>("password1", "password2")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val btn = findViewById<Button>(R.id.signupbtn)
-        var bundle = Bundle()
-        btn.setOnClickListener {
-            val user = findViewById<EditText>(R.id.username)
-            val userInput = user.text.toString()
-            var result = "user successfully registered"
-            if (userInput in users) {
-                result = "user already registered. Insert the password"
-
-            } else {
+        val users = arrayOf("User0","User1","User2","User3","User4")
+        val passwords = arrayOf("A","B","C","D","E")
 
 
-                val email = findViewById<EditText>(R.id.email)
-                val emailInput = email.text.toString()
-                val password = findViewById<EditText>(R.id.password)
-                val passwordInput = password.text.toString()
-                val repassword = findViewById<EditText>(R.id.repassword)
-                val repasswordInput = repassword.text.toString()
+        val user = findViewById<EditText>(R.id.user)
+        var userInput = user.text.toString()
+        val password = findViewById<EditText>(R.id.password)
+        var passwordInput = password.text.toString()
+        var result = findViewById<TextView>(R.id.resultado)
+        var res = "resultado"
 
-                if (passwordInput.equals(repasswordInput)) {
-                    users.add(userInput)
-                    emails.add(emailInput)
-                    passwords.add(passwordInput)
-                } else {
-                    result = "Password does not match. Retype the password"
+        var button = findViewById<Button>(R.id.login)
+        button.setOnClickListener(){
+            if (userInput in users){
+                if (passwordInput in passwords){
+                    res = "Login efetuado"
+                    result.setText(res)
                 }
             }
+            else {
+                res = "Usuário não existente"
+                result.setText(res)
+            }
+        }
 
-        bundle.putString("res",result)
-        val intent = Intent(this, ResultActivity::class.java)
-        intent.putExtras(bundle)
-            startActivity(intent)
+        var buttonC = findViewById<Button>(R.id.clear);
+        buttonC.setOnClickListener(){
+            userInput = ""
+            passwordInput = ""
+            res = "resultado"
+            result.setText(res)
         }
     }
 }
